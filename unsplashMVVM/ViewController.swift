b495c181ed81729c91f7626c13cee3cd919d6187
hyeseong <hyeseong7848@gmail.com>
2023-09-11 12:33:58 +0900

    다국어대응 (앱 내에 텍스트, 앱이름, 권한요청 텍스트 등)

--- unsplashMVVM/ViewController.swift
+++ unsplashMVVM/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nicknameTextField: UITextField!
+    @IBOutlet var changeButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nicknameTextField.placeholder = "nickname_placeholder".localized
+        
+        //MARK: %@ -> String 
+//        let value = NSLocalizedString("nickname_result", comment: "")
+//        resultLabel.text = String(format: value, "고래밥", "상어", "라면")
+        
+        resultLabel.text = "age_result".localized(number: 3)
+        
+        // cmd ctrl e
+        let searchBar = UISearchBar()
+        searchBar.text = "asd"
+        searchBar.placeholder = "sds"
+        searchBar.searchTextField.text = "sdsd"
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#function)        // Ctrl + Shift + 클릭 => 동시에 입력
+    }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)        // Ctrl + Shift + 클릭 => 동시에 입력
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)        // Ctrl + Shift + 클릭 => 동시에 입력
+    }
 }
 
